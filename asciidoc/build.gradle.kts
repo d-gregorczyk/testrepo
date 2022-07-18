a2dbcc1d1eb1e1a7788499ce6f9216ee605ec3d1
Gregorczyk <David.Gregorczyk@draeger.com>
2022-07-18 15:40:08 +0200

    Add input files and conversion code.

--- asciidoc/build.gradle.kts
+++ asciidoc/build.gradle.kts
@@ -1,6 +1,7 @@
 import org.jetbrains.kotlin.gradle.tasks.KotlinCompile
 
 plugins {
+    java
     kotlin("jvm") version "1.6.21"
     application
 }
@@ -14,13 +15,23 @@ repositories {
 
 dependencies {
 
+    // https://mvnrepository.com/artifact/com.github.ajalt/clikt
+    implementation(group = "com.github.ajalt", name = "clikt", version = "2.8.0")
+
     // https://mvnrepository.com/artifact/org.asciidoctor/asciidoctorj
     implementation("org.asciidoctor:asciidoctorj:2.5.4")
+    implementation("org.asciidoctor:asciidoctorj-pdf:2.1.4")
+    implementation("org.asciidoctor:asciidoctorj-diagram:2.2.3")
+    implementation("org.asciidoctor:asciidoctorj-diagram-plantuml:1.2022.5")
     testImplementation(kotlin("test"))
 }
 
 tasks.test {
     useJUnitPlatform()
+}
+
+tasks.named<JavaCompile>("compileJava") {
+    options.compilerArgs = listOf("--add-opens", "java.base/sun.nio.ch=ALL-UNNAMED")
 }
 
 tasks.withType<KotlinCompile> {
--- asciidoc/src/main/kotlin/Main.kt
+++ asciidoc/src/main/kotlin/Main.kt
@@ -1,8 +1,39 @@
-fun main(args: Array<String>) {
-    println("Hello World!")
+import com.github.ajalt.clikt.core.CliktCommand
+import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
+import com.github.ajalt.clikt.parameters.options.validate
+import com.github.ajalt.clikt.parameters.types.file
+import org.asciidoctor.Asciidoctor
+import org.asciidoctor.Options
+import org.asciidoctor.SafeMode
+import java.io.File
 
-    // Try adding program arguments via Run/Debug configuration.
-    // Learn more about running applications: https://www.jetbrains.com/help/idea/running-applications.html.
-    println("Program arguments: ${args.joinToString()}")
-    println("Yeeehaaa")
+fun main(args: Array<String>) = PublishSdpi().main(args)
+
+class PublishSdpi : CliktCommand("publish-sdpi") {
+    private val adocInputFile by option("--input-file", help = "path to ascii doc input file")
+        .file()
+        .required()
+        .validate {
+            require(it.exists()) { "Input file '$it' does not exist." }
+        }
+
+    override fun run() {
+        val inputFile = adocInputFile
+
+        println("Start conversion of ${inputFile.absolutePath}")
+
+        val options = Options.builder()
+            .safe(SafeMode.UNSAFE)
+            .backend("pdf")
+            .toFile(File("../output.pdf"))
+            .build()
+
+        val asciidoctor = Asciidoctor.Factory.create()
+        asciidoctor.requireLibrary("asciidoctor-diagram");
+        asciidoctor.convertFile(inputFile, options)
+
+        asciidoctor.shutdown()
+    }
 }
+
