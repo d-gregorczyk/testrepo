62752ddc511c86ffcc21a56b782aee8562643c67
David Gregorczyk <david.gregorczyk@web.de>
2022-07-22 23:39:57 +0200

    Meh

--- asciidoc/src/main/kotlin/org/sdpi/Main.kt
+++ asciidoc/src/main/kotlin/org/sdpi/Main.kt
@@ -1,6 +1,7 @@
 import com.github.ajalt.clikt.core.CliktCommand
 import com.github.ajalt.clikt.parameters.options.default
 import com.github.ajalt.clikt.parameters.options.option
+import com.github.ajalt.clikt.parameters.options.required
 import com.github.ajalt.clikt.parameters.options.validate
 import com.github.ajalt.clikt.parameters.types.file
 import org.apache.logging.log4j.Level
@@ -24,23 +25,26 @@ class PublishSdpi : CliktCommand("publish-sdpi") {
     private companion object : Logging
 
     // for some reason, github actions do not digest double quotes correctly right now - requires hard coded config
-    private val adocInputFile by option("--input-file", help = "path to ascii doc input file").file().default(
-        File("../sdpi-supplement.adoc")
-    )
+    private val adocInputFile by option("--input-file", help = "path to ascii doc input file").file().required()
         .validate {
             require(it.exists()) { "Input file '$it' does not exist." }
         }
-//
-//    private val outputFolder by option("--output-folder", help = "path to artifact doc output folder").file().required()
-//        .validate {
-//            require(it.parentFile.exists()) { "Output parent folder '${it.parentFile.absolutePath}' does not exist." }
-//            if (!it.exists()) {
-//                require(it.mkdir()) { "Output folder '${it.absolutePath}' could not be created" }
-//            }
 
-    //private val adocInputFile = File("../sdpi-supplement.adoc")
-    private val outputFolder = File("../sdpi-supplement")
-    private val backend = "pdf" // html
+    private val outputFolder by option("--output-folder", help = "path to artifact doc output folder").file().required()
+        .validate {
+            require(it.parentFile.exists()) { "Output parent folder '${it.parentFile.absolutePath}' does not exist." }
+            if (!it.exists()) {
+                require(it.mkdir()) { "Output folder '${it.absolutePath}' could not be created" }
+            }
+        }
+
+    private val backend by option("--backend", help = "'pdf' (default) or 'html' to generate output accordingly")
+        .default("pdf")
+        .validate {
+            require(it == "pdf" || it == "html") {
+                "Backend needs to be pdf or html, but is: $it"
+            }
+        }
 
     override fun run() {
         Configurator.initialize(ConfigurationBuilderFactory.newConfigurationBuilder().let {
