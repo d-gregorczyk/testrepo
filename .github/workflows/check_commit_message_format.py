267f50ae7770409f56c0d02ce0c9718fc020e6bf
David Gregorczyk <david.gregorczyk@web.de>
2023-08-28 14:48:57 +0200

    change code

--- .github/workflows/check_commit_message_format.py
+++ .github/workflows/check_commit_message_format.py
@@ -1,4 +1,5 @@
 import os
 import sys
 
-os.system('git log --left-right --graph --cherry-mark --oneline master...' + sys.argv[1])
+# os.system('git log --left-right --graph --cherry-mark --oneline master...' + sys.argv[1])
+os.system('git status')
