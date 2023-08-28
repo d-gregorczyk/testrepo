ca0ac17da2d85552b68250d2194c702db8e66152
David Gregorczyk <david.gregorczyk@web.de>
2023-08-28 15:21:33 +0200

    change code

--- .github/workflows/check_commit_message_format.py
+++ .github/workflows/check_commit_message_format.py
@@ -3,6 +3,7 @@ import sys
 
 os.system('git fetch origin')
 os.system('git checkout master')
+os.system('git pull')
 os.system('git checkout ' + sys.argv[1])
 os.system('git rev-list --ancestry-path master..' + sys.argv[1])
 output = os.popen('git rev-list --ancestry-path master..' + sys.argv[1]).read()
