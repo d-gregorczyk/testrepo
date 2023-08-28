418525c0379fb79da2b1a31109bc280ad07e2d53
David Gregorczyk <david.gregorczyk@web.de>
2023-08-28 16:41:34 +0200

    change code

--- .github/workflows/check_commit_message_format.py
+++ .github/workflows/check_commit_message_format.py
@@ -1,13 +1,19 @@
 import os
 import sys
+from git import Repo
 
-os.system('git fetch origin')
-os.system('git checkout master')
-os.system('git pull')
-os.system('git checkout ' + sys.argv[1])
-os.system('git rev-list --ancestry-path master..' + sys.argv[1])
-output = os.popen('git rev-list --ancestry-path master..' + sys.argv[1]).read()
+# os.system('git fetch origin')
+# os.system('git checkout master')
+# os.system('git pull')
+# os.system('git checkout ' + sys.argv[1])
+# os.system('git rev-list --ancestry-path master..' + sys.argv[1])
+# output = os.popen('git rev-list --ancestry-path master..' + sys.argv[1]).read()
 
-for git_hash in output.split():
-    print(git_hash)
+# for git_hash in output.split():
+#     print(git_hash)
 
+repo = Repo("../..")
+git = repo.git
+git.checkout("master")
+output = git.rev_list("--ancestry_path", "master.." + sys.argv[1])
+print(output)
