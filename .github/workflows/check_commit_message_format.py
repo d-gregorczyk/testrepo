a2e2319f117fd0b38cce35b11227b6ea1abea976
David Gregorczyk <david.gregorczyk@web.de>
2023-08-28 17:12:11 +0200

    change code

--- .github/workflows/check_commit_message_format.py
+++ .github/workflows/check_commit_message_format.py
@@ -1,26 +1,24 @@
-import os
+import re
 import sys
+
 from git import Repo
 
-# os.system('git fetch origin')
-# os.system('git checkout master')
-# os.system('git pull')
-# os.system('git checkout ' + sys.argv[1])
-# os.system('git rev-list --ancestry-path master..' + sys.argv[1])
-# output = os.popen('git rev-list --ancestry-path master..' + sys.argv[1]).read()
 
-# for git_hash in output.split():
-#     print(git_hash)
+def eprint(*args, **kwargs):
+    print(*args, file=sys.stderr, **kwargs)
+
 
 repo = Repo(".")
 git = repo.git
-#print(git.fetch("origin", sys.argv[1]))
-#print(git.checkout(sys.argv[1]))
-#print(git.fetch("origin", "master"))
-#print(git.checkout("master"))
-#print(git.checkout(sys.argv[1]))
 output = git.rev_list("--ancestry-path", "origin/master.." + sys.argv[1])
-#output = git.rev_list("origin..HEAD")
-print(git.branch())
+found_invalid_commit_messages = False
+
 for git_hash in output.split():
-    print(git_hash)
+    commit_message = git.log("-1", "--pretty=format:%s", git_hash)
+    if re.search('^#\\d+\\s.+', commit_message) is None:
+        eprint("Commit {} does not contain a valid commit message. Please fix before merge".format(git_hash))
+        eprint("Commit message format: '#<issue-id> <text>'")
+        found_invalid_commit_messages = True
+
+if found_invalid_commit_messages:
+    sys.exit(1)
